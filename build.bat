565a43df7e76e37691923fd44f73e09cd5f6ac33
spetz <spetz90@gmail.com>
2017-03-01 11:30:10 +0100

    Fixed dotnet pack

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 dotnet restore --source "https://api.nuget.org/v3/index.json"
-dotnet pack "Collectively.Messages" -o .
+dotnet pack -o .
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dotnet restore --source "https://api.nuget.org/v3/index.json"
-dotnet pack "Collectively.Messages" -o .
+dotnet pack -o .
