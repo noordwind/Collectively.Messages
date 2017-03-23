737a3c5dea1474b70be137fef51846e0e7368332
spetz <spetz90@gmail.com>
2017-03-23 12:57:50 +0100

    Created at remark comment property

--- Events/Remarks/CommentEditedInRemark.cs
+++ Events/Remarks/CommentEditedInRemark.cs
@@ -9,19 +9,22 @@ namespace Collectively.Messages.Events.Remarks
         public Guid RemarkId { get; }
         public Guid CommentId { get; }
         public string Text { get; }
+        public DateTime CreatedAt { get; }
 
         protected CommentEditedInRemark()
         {
         }
 
         public CommentEditedInRemark(Guid requestId, string userId,
-            Guid remarkId, Guid commentId, string text)
+            Guid remarkId, Guid commentId, string text,
+            DateTime createdAt)
         {
             RequestId = requestId;
             UserId = userId;
             RemarkId = remarkId;
             CommentId = commentId;
             Text = text;
+            CreatedAt = createdAt;
         }
     }
 }
