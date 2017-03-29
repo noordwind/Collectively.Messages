06668a09a7bc8c6ffe9bee4952fb1188defd12ca
spetz <spetz90@gmail.com>
2017-03-29 11:52:00 +0200

    Comment id for new add comment message

--- Commands/Remarks/AddCommentToRemark.cs
+++ Commands/Remarks/AddCommentToRemark.cs
@@ -6,6 +6,7 @@ namespace Collectively.Messages.Commands.Remarks
     {
         public Request Request { get; set; }
         public string UserId { get; set; }
+        public Guid CommentId { get; set; }
         public Guid RemarkId { get; set; }
         public string Text { get; set; }
     }
