1067563e0fe295cd174b4ea3d9b47bae151b52d9
spetz <spetz90@gmail.com>
2017-04-05 13:25:49 +0200

    Taken action date

--- Events/Remarks/RemarkActionTaken.cs
+++ Events/Remarks/RemarkActionTaken.cs
@@ -9,19 +9,22 @@ namespace Collectively.Messages.Events.Remarks
         public string Username { get; }
         public Guid RemarkId { get; }
         public string Description { get; }
+        public DateTime CreatedAt { get; }
 
         protected RemarkActionTaken()
         {
         }
 
         public RemarkActionTaken(Guid requestId, string userId,
-            string username, Guid remarkId, string description)
+            string username, Guid remarkId, string description,
+            DateTime createdAt)
         {
             RequestId = requestId;
             UserId = userId;
             Username = username;
             RemarkId = remarkId;
             Description = description;
+            CreatedAt = createdAt;
         }
     }
 }
