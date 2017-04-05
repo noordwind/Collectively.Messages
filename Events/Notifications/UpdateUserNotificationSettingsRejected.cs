ac49928c288c68d7a71756e36f6b5890d75d8593
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2017-04-05 12:49:07 +0200

    redundant property removed from update user notification rejected event

--- Events/Notifications/UpdateUserNotificationSettingsRejected.cs
+++ Events/Notifications/UpdateUserNotificationSettingsRejected.cs
@@ -5,7 +5,6 @@ namespace Collectively.Messages.Events.Notifications
     public class UpdateUserNotificationSettingsRejected : IRejectedEvent
     {
         public Guid RequestId { get; }
-        public Guid RemarkId { get; }
         public string UserId { get; }
         public string Reason { get; }
         public string Code { get; }
@@ -16,11 +15,9 @@ namespace Collectively.Messages.Events.Notifications
         }
 
         public UpdateUserNotificationSettingsRejected(Guid requestId,
-            Guid remarkId, string userId,
-            string code, string reason)
+            string userId, string code, string reason)
         {
             RequestId = requestId;
-            RemarkId = remarkId;
             UserId = userId;
             Code = code;
             Reason = reason;
