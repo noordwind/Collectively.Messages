5415b78432670c6fa2a44717dae3ace7aa194dfa
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2017-06-19 16:17:32 +0200

    file upload command interface updated

--- Commands/IFileUploadCommand.cs
+++ Commands/IFileUploadCommand.cs
@@ -1,9 +1,13 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace Collectively.Messages.Commands
 {
-    public interface IFileUploadCommand
+    public interface IFileUploadCommand : ICommand
     {
+        string Title { get; set; }
+        string Description { get; set; }
+        IEnumerable<string> Tags { get; set; }
         Stream File { get; set; }
     }
 }
