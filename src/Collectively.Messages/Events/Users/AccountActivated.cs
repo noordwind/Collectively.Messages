941e38a45556e7a568e58c073a89f3e4db7a8a61
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2017-08-02 21:52:51 +0200

    activate account events updated

--- src/Collectively.Messages/Events/Users/AccountActivated.cs
+++ src/Collectively.Messages/Events/Users/AccountActivated.cs
@@ -6,14 +6,16 @@ namespace Collectively.Messages.Events.Users
     {
         public Guid RequestId { get; }
         public string UserId { get; }
+        public string Email { get; set; }
 
         protected AccountActivated()
         {
         }
 
-        public AccountActivated(Guid requestId, string userId)
+        public AccountActivated(Guid requestId, string email, string userId)
         {
             RequestId = requestId;
+            Email = email;
             UserId = userId;
         }
     }
