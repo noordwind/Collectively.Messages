936e54c9a4fa0a3d85ec237f814895a714c0d64b
Piotr Gankiewicz <spetz90@gmail.com>
2017-08-19 17:56:56 +0200

    Custom package build script

--- scripts/after-success.sh
+++ scripts/after-success.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+echo Executing after success scripts on branch $TRAVIS_BRANCH
+echo Triggering MyGet package build
+
 cd src/Collectively.Messages
 dotnet pack /p:PackageVersion=2.0.$TRAVIS_BUILD_NUMBER --no-restore -o .
 
+echo Uploading MyGet package build using branch $TRAVIS_BRANCH
+
 case "$TRAVIS_BRANCH" in
   "master")
-    echo Uploading MyGet package build using branch $TRAVIS_BRANCH
     dotnet nuget push *.nupkg -k $MYGET_API_KEY -s https://www.myget.org/F/collectively/api/v2/package
     ;;
   "develop")
-    echo Triggering MyGet package build using branch $TRAVIS_BRANCH
     dotnet nuget push *.nupkg -k $MYGET_DEV_API_KEY -s https://www.myget.org/F/collectively-dev/api/v2/package
     ;;    
 esac
