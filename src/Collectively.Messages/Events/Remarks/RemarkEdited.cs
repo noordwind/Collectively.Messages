f09ee0056e99c863b09036c942838128d8a034bb
Piotr Gankiewicz <spetz90@gmail.com>
2017-10-05 13:06:17 +0200

    Resource for remark edited

--- src/Collectively.Messages/Events/Remarks/RemarkEdited.cs
+++ src/Collectively.Messages/Events/Remarks/RemarkEdited.cs
@@ -5,6 +5,7 @@ namespace Collectively.Messages.Events.Remarks
     public class RemarkEdited : IAuthenticatedEvent, IEditable
     {
         public Guid RequestId { get; }
+        public Resource Resource { get; }
         public string UserId { get; }
         public Guid RemarkId { get; }
 
@@ -13,9 +14,11 @@ namespace Collectively.Messages.Events.Remarks
         }
 
         public RemarkEdited(Guid requestId, 
-            string userId, Guid remarkId)
+            Resource resource, string userId, 
+            Guid remarkId)
         {
             RequestId = requestId;
+            Resource = resource;
             UserId = userId;
             RemarkId = remarkId;
         }
